2bc2ed9dc953b52b5c0c90f32fc4ece9f097fff1
riderboydr12 <dineshvmdl1@gmail.com>
2024-02-02 07:20:08 -1200

    firstcommit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dice-game---Project-3
+Hello, This is a Dice game project that is assigned to me by GUVI's 3rd Project using HTML, CSS, JavaScript and DOM
---
+++ script.js
@@ -0,0 +1,92 @@
+function random1() {
+    const rand = Math.floor(Math.random() * 6) + 1;
+    return rand;
+}
+function random2() {
+    const rand = Math.floor(Math.random() * 6) + 1;
+    return rand;
+}
+
+function loadran() {
+    const dice = document.getElementById("die");
+    dice.src = "./images/" + random1() + ".png"
+}
+const randload = Math.floor(Math.random() * 2) + 1;
+function load() {
+    if (randload === 1) {
+        button1.setAttribute("disabled", " ")
+        loadran();
+    } else {
+        button2.setAttribute("disabled", " ")
+        loadran();
+    }
+}
+load();
+button1 = document.getElementById("button1");
+button1.addEventListener("click", rolldie1);
+
+button2 = document.getElementById("button2");
+button2.addEventListener("click", rolldie2);
+
+function rolldie1() {
+    let ran1 = random1();
+    const dice = document.getElementById("die");
+    dice.src = "./images/" + ran1 + ".png"
+    button1.setAttribute("disabled", "");
+    button2.removeAttribute("disabled");
+    var initial1 = document.getElementById("p1-id").innerHTML;
+    var add1 = parseInt(initial1) + parseInt(ran1);
+    document.getElementById("p1-id").innerHTML = add1;
+    document.getElementById("h1").innerHTML = "Player-2 to Play"
+    if (add1 >= 30) {
+        button1.disabled = true;
+        button2.disabled = true;
+        reset.disabled = false;
+        const win = document.getElementById("h1");
+        win.innerHTML = "Player-1 Won the Game 🥳🎆💯";
+    }
+}
+
+function rolldie2() {
+    let ran2 = random2();
+    const dice = document.getElementById("die");
+    dice.src = "./images/" + ran2 + ".png"
+    button2.setAttribute("disabled", "");
+    button1.removeAttribute("disabled");
+    var initial2 = document.getElementById("p2-id").innerHTML;
+    var add2 = parseInt(initial2) + parseInt(ran2);
+    document.getElementById("p2-id").innerHTML = add2;
+    document.getElementById("h1").innerHTML = "Player-1 to Play"
+    if (add2 >= 30) {
+        button1.disabled = true;
+        button2.disabled = true;
+        reset.disabled = false;
+        const win = document.getElementById("h1");
+        win.innerHTML = "Player-2 Won the Game 🥳🎆💯";
+    }
+}
+
+const reset = document.getElementById("rst");
+reset.disabled = true;
+reset.addEventListener("click", () => {
+    document.getElementById("p1-id").innerHTML = 0;
+    document.getElementById("p2-id").innerHTML = 0;
+    const random = Math.floor(Math.random() * 2) + 1
+    if (random === 1) {
+        button1.disabled = false;
+    } else {
+        button2.disabled = false;
+    }
+    reset.disabled = true
+    if (button1.disabled == false) {
+        document.getElementById("h1").innerHTML = "Player-1 to Play"
+    } else {
+        document.getElementById("h1").innerHTML = "Player-2 to Play"
+    }
+})
+
+if (button1.disabled == false) {
+    document.getElementById("h1").innerHTML = "Player-1 to Play"
+} else {
+    document.getElementById("h1").innerHTML = "Player-2 to Play"
+}
